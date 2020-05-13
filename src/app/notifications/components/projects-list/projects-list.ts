87c30c449a38a9f7e3be4e8bdc60f6354232edef
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-05-13 13:26:14 +0200

    adding projects-list.scss

--- src/app/notifications/components/projects-list/projects-list.ts
+++ src/app/notifications/components/projects-list/projects-list.ts
@@ -3,7 +3,8 @@ import { Project } from '../../models/project.model';
 
 @Component({
     selector: 'projects-list',
-    templateUrl: './projects-list.html'
+    templateUrl: './projects-list.html',
+    styleUrls: ['./projects-list.scss']
 })
 export class ProjectsListComponent{
     @Input('projects') projects: Project[] = [];
