cacc30e8c7b5334667934e84e1a9ee6fb03d2ca9
Pierre Sid-idris <pierre.sididris@live.fr>
2020-07-14 16:02:48 +0200

    button to select git platform

--- src/app/webhook/components/liste-platform/liste-platform.component.ts
+++ src/app/webhook/components/liste-platform/liste-platform.component.ts
@@ -1,5 +1,7 @@
 
 import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'ci-liste-platform',
@@ -9,9 +11,38 @@ import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 export class ListePlatformComponent implements OnInit {
 
 
-  checked: boolean;
-  @Output() changed = new EventEmitter<boolean>();
-  constructor() { }
+  form: FormGroup;
+  Data: Array<any> = [
+    { name: 'Github', value: 'github' },
+    { name: 'GitLab', value: 'gitlab' },
+    { name: 'Bitbucket', value: 'bitbucket' },
+  ];
+  constructor(private fb: FormBuilder) { 
+    this.form = this.fb.group({
+      checkArray: this.fb.array([], [Validators.required])
+    })
+  }
+
+  onCheckboxChange(e) {
+    const checkArray: FormArray = this.form.get('checkArray') as FormArray;
+
+    if (e.target.checked) {
+      checkArray.push(new FormControl(e.target.value));
+    } else {
+      let i: number = 0;
+      checkArray.controls.forEach((item: FormControl) => {
+        if (item.value == e.target.value) {
+          checkArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+  }
+
+  submitForm() {
+    console.log(this.form.value)
+  }
   // ngOnInit () {  }
 
 
